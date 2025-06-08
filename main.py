89b143a479d3541fc2da1ed3b28df706dc444251
Gamitasvp <LorenzoBlas>
2025-06-07 20:54:32 -0600

    Commit act_2 07/06/2025

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import sys
 from src.act_1 import continuous_signals, discrete_signals, plot_all_signals
+from src.act_2 import continuous_sin, sine_plotter
 
 def main(arg):
-    if arg[1] == "act_1":
-        
-        
+    if arg[1] == "act_1":  
         time_cont, signals_cont = continuous_signals()
         time_disc, signals_disc = discrete_signals()
         plot_all_signals(time_cont, signals_cont, time_disc, signals_disc)
-    #elif arg[1] == "act_2":
+    elif arg[1] == "act_2":
+        if len(arg) > 2:
+            try:
+                frequency = 0
+                frequency = int(arg[2])
+                sine_signal, time = continuous_sin(frequency)
+                sine_plotter(sine_signal,frequency,time)
+            except ValueError:
+                print("Posible argumento fuera de los limites de la programacion (asegurese que el segundo argumento sea un numero entero")
+         
+        
     #elif arg[1] == "act_3":
     #elif arg[1] == "act:4":
 
